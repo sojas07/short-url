90db026a5cb196d3495111c352c4f72c905d711c
sojas07 <sojaskhandagale@gmail.com>
2021-09-11 19:33:48 +0530

    sojas

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require('express')
+const shortId = require('shortid')
+const createHttpError = require('http-errors')
+const mongoose = require('mongoose')
+const path = require('path')
+const ShortUrl = require('./models/url')
+
+const app = express()
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+mongoose
+  .connect('mongodb://localhost:27017', {
+    dbName: 'url-shortner',
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log('mongoose connected 💾'))
+  .catch((error) => console.log('Error connecting..'))
+
+app.set('view engine', 'ejs')
+
+app.get('/', async (req, res, next) => {
+  res.render('index')
+})
+
+app.post('/', async (req, res, next) => {
+  try {
+    const { url } = req.body
+    //console.log(url+"hello");
+    if (!url) {
+      throw createHttpError.BadRequest('Provide a valid url')
+    }
+    const urlExists = await ShortUrl.findOne({ url })
+    if (urlExists) {
+      res.render('index', {
+        short_url: `http://localhost:4000/${urlExists.shortId}`,
+      })
+      return
+    }
+    const shortUrl = new ShortUrl({ url: url, shortId: shortId.generate() })
+    const result = await shortUrl.save()
+    res.render('index', {
+      // short_url: `${req.hostname}/${urlExists.shortId}`, //if we have domain then we can use req.hostname
+      short_url: `http://localhost:4000/${result.shortId}`, // i am not having any domain so I am using local host
+    })
+  } catch (error) {
+    next(error)
+  }
+})
+
+app.get('/:shortId', async (req, res, next) => {
+  try {
+    const { shortId } = req.params
+    const result = await ShortUrl.findOne({ shortId })
+    if (!result) {
+      throw createHttpError.NotFound('Short url does not exist')
+    }
+    res.redirect(result.url)
+  } catch (error) {
+    next(error)
+  }
+})
+
+app.use((req, res, next) => {
+  next(createHttpError.NotFound())
+})
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  res.render('index', { error: err.message })
+})
+
+app.listen(4000, () => console.log('started on port 4000...'))
